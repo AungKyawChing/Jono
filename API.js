1158d468a9780a632148bce92f88aa8632d1d1a9
AungKyawChing <122950055+AungKyawChing@users.noreply.github.com>
2026-02-02 03:35:07 -0800

    Update WEB_APP_URL to new endpoint

--- API.js
+++ API.js
@@ -1,3 +1,3 @@
 const WEB_APP_URL = `
-https://script.google.com/macros/s/AKfycbwDFZ-82RR45XqjrCkuPN02tMgFchBeQPOF3YZOzK87H8YYAV8shQSxoFqSrTQvZYqu/exec
+  https://script.google.com/macros/s/AKfycbzSBZDZSJPN7SsObggNHwL23lykIJjGE9Eb5X2CVJO6jGriCZ-WIpWRgFEdMkKfwADc/exec
 `;
